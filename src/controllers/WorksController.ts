f235fe2b48fecfc61622241d037c3f243790d01e
dilan Espindola <dilanespindola30@gmail.com>
2022-07-05 12:58:13 -0500

    api RESFUL completed for works

--- src/controllers/WorksController.ts
+++ src/controllers/WorksController.ts
@@ -12,6 +12,19 @@ export const getWork = async (_req: Request, res: Response) => {
   }
 };
 
+export const getWorkById = async (req: Request, res: Response) => {
+  try {
+    const workById = await Work.findById(req.params.id);
+    if (workById) {
+      res.json(workById);
+    } else {
+      res.json({ msg: "Work not found" });
+    }
+  } catch ({ message }) {
+    res.status(500).json({ error: message });
+  }
+};
+
 export const postWork = async (req: Request, res: Response) => {
   const { projectName, description, gitHubUrl, image, pageUrl, userId } =
     req.body as WorkSchema;
@@ -30,6 +43,24 @@ export const postWork = async (req: Request, res: Response) => {
       $push: { works: workSaved._id },
     })) as UserSchema;
     res.status(200).json({ msg: "Work have been saved" });
+  } catch ({ message }) {
+    res.status(500).json({ error: message });
+  }
+};
+
+export const updateWork = async (req: Request, res: Response) => {
+  try {
+    await Work.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ msg: "Work have been updated" });
+  } catch ({ message }) {
+    res.status(500).json({ error: message });
+  }
+};
+
+export const deleteWork = async (req: Request, res: Response) => {
+  try {
+    await Work.findByIdAndDelete(req.params.id);
+    res.json({ msg: "Work have been deleted" });
   } catch ({ message }) {
     res.status(500).json({ error: message });
   }
--- src/routes/works.ts
+++ src/routes/works.ts
@@ -3,6 +3,9 @@ import * as worksController from "../controllers/WorksController";
 const route = Router();
 
 route.get("/", worksController.getWork);
+route.get("/:id", worksController.getWorkById);
 route.post("/", worksController.postWork);
+route.put("/:id", worksController.updateWork);
+route.delete("/:id", worksController.deleteWork);
 
 export default route;
