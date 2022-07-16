5f0f2a3c6179896bd4dd14c6399c1c873f8424b4
dilan Espindola <dilanespindola30@gmail.com>
2022-07-16 18:50:33 -0500

    minor change

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,10 +5,6 @@ import { User as NewUser, UserSchema } from "../interfaces/interfaces";
 import User from "../Models/User";
 import config from "../config";
 
-interface JWTPayload {
-  id: string;
-}
-
 export const userGet = async (_req: Request, res: Response) => {
   try {
     const users = await User.find().populate("works");
@@ -29,9 +25,11 @@ export const userRegister = async (req: Request, res: Response) => {
     const token = jwt.sign({ id: userSaved._id }, config.JWT_SECRET as string, {
       expiresIn: "24h",
     });
-    return res
-      .status(200)
-      .json({ msg: "User have been saved", token, user: userSaved });
+    return res.status(200).json({
+      token,
+      user: userSaved,
+      auth: true,
+    });
   } catch (error) {
     throw new Error(error as string);
   }
@@ -41,12 +39,13 @@ export const userLogin = async (req: Request, res: Response) => {
   const { email, password } = req.body as NewUser;
   try {
     const getUser = await User.findOne({ email });
-    if (!getUser) return res.json({ msg: "email is not valid" });
+    if (!getUser) return res.json({ msg: "email is not valid", auth: false });
 
     const getUserPassword = getUser.password;
     const validatePassword = await bcrypt.compare(password, getUserPassword);
 
-    if (!validatePassword) return res.json({ msg: "wrong password" });
+    if (!validatePassword)
+      return res.json({ msg: "wrong password", auth: false });
     const token = jwt.sign({ id: getUser._id }, config.JWT_SECRET as string, {
       expiresIn: "24h",
     });
